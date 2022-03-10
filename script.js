bbfd3d7ff6e7b9cc0435633c57f8b3102dec06d4
razz1000 <76906581+razz1000@users.noreply.github.com>
2022-03-10 16:04:52 +0000

    Works now for both bingo top board and the user board below

--- script.js
+++ script.js
@@ -22,20 +22,26 @@ createBingoNumbers();
 //This below is the function for the buttonClickRandomNumberGenerator   
  let buttonClickRandomNumberGenerator = () => {
     let newBingoNumber = document.querySelectorAll(".bingo-number")
+    let newUserBoardNumber = document.querySelectorAll(".userboard-number")
  
     function getRandomNumber() { 
         var randomNumber = Math.floor( Math.random() * 76 ) + 1; return randomNumber; } 
         let dieroll = getRandomNumber();
 
-
+        /* This is the for loop for the top Bingo board  */
     for (let i = 1; i < newBingoNumber.length; i++) {
     if (dieroll === parseInt(newBingoNumber[i].innerText)) {
         newBingoNumber[i].classList.add("highlighted-cell")
-        buttonClickRandomNumberGenerator;            
     }
     }
+    /* This is the for loop for the userboard  */
+    for (let i = 1; i < newUserBoardNumber.length; i++) {
+    if(dieroll === parseInt(newUserBoardNumber[i].innerText)) {
+        newUserBoardNumber[i].classList.add("highlighted-cell")
+        buttonclickOnBoardgenerator; 
 }
-
+}
+}
 
 // Creating the 24 userboard numbers.
 let totalUserBoardNumbers = 24;
@@ -51,7 +57,12 @@ let createUserBoard = () => {
 }
 createUserBoard();
 
-/* 
+
+
+
+
+
+/* This was the old way I did it: 
 let buttonclickOnBoardgenerator = () => {   
     let newUserBoardNumber = document.querySelectorAll(".userboard-number")
      
