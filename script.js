87d6a2a34d7328f23d15ec60bd363f5f6cb7a3fb
razz1000 <76906581+razz1000@users.noreply.github.com>
2022-03-10 15:15:04 +0000

    The button is working
    - generating a randum number and highlighting it

--- script.js
+++ script.js
@@ -19,3 +19,50 @@ let createBingoNumbers = () => {
     }
 }
 createBingoNumbers();
+
+
+// Function for callling the random number 
+let randomNumberGenerated = Math.floor(Math.random() * 76);
+
+
+
+
+  
+ let buttonClickRandomNumberGenerator = () => {
+    let newBingoNumber = document.querySelectorAll(".bingo-number")
+
+
+    function getRandomNumber() { 
+        var randomNumber = Math.floor( Math.random() * 76 ) + 1; return randomNumber; } 
+        let dieroll = getRandomNumber();
+
+    for (let i = 1; i < newBingoNumber.length; i++) {
+    if (dieroll === parseInt(newBingoNumber[i].innerText)) {
+
+        newBingoNumber[i].classList.add("highlighted-cell")
+        buttonClickRandomNumberGenerator;            
+    }
+
+    }
+}
+   
+
+/* 
+let buttonClickRandomNumberGenerator = () => {
+    let newBingoNumber = document.querySelectorAll(".bingo-number")[0]
+    newBingoNumber.classList.add("highlighted-cell")
+    
+}
+ */
+
+
+/*  
+    var random;
+    var max = 8
+    function findRandom() {
+      random = Math.floor(Math.random() * max) //Finds number between 0 - max
+      console.log(random)
+    }
+    findRandom();
+
+    let randomNumber = findRandom(). */
